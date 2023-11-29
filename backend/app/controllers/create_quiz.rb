ebff754b14d83e558bd4eddba91273af2cca432f
haga_hiroki <ragna.1123@gmail.com>
2023-11-30 00:30:41 +0900

    適当なテストデータ作成　IDをランダムに取れる処理

--- backend/app/controllers/create_quiz.rb
+++ backend/app/controllers/create_quiz.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 require 'webrick'
 require 'json'
 require 'mysql2'
 require 'dotenv'
 Dotenv.load
 
+# 名前にサーブレットつけたクラスはAPIに関するもの
 class CreateQuizServlet < WEBrick::HTTPServlet::AbstractServlet
   def do_GET(_req, res)
     res.status = 200
@@ -11,39 +14,47 @@ class CreateQuizServlet < WEBrick::HTTPServlet::AbstractServlet
     res['Access-Control-Allow-Origin'] = ENV['CLIENT_SERVER']
     # quiz = ResponsQuiz.new() # ここの引数にレベルが入る
     req_quiz_level = _req.query # リクエストのパラメータ(レベル)を取得
-    level_value = req_quiz_level["quiz_level"]
-    # CreateQuiz.new(level_value) # レベルを元にクイズを生成
-    p level_value
-    res.body = level_value.to_json
+    level_value = req_quiz_level['quiz_level']
+
+    create_quiz = CreateQuizId.new(level_value) # レベルを元にクイズを生成
+    res.body = create_quiz.get_db_quiz_id #bodyへテスト
   end
 end
 
-class Quiz
+class CreateQuiz
   attr_accessor :quiz_id_array
+
   def initialize
     @client = Mysql2::Client.new(
       username: ENV['DB_USER'],
       password: ENV['DB_PASSWORD'],
       database: ENV['DB_NAME']
     ) # クイズインスタンが生成されたタイミングでDBへの接続設定を行う　子クラスでは継承注意
-    @quiz_id_array = [] #ここに５個クイズのIDが保存される
+    @quiz_id_array = [] # ここに５個クイズのIDが保存される
+    @quiz_content = {} # 以下ハッシュのキー
+    # question, question_png_path, question_ans_gif, options, explanation, question_level, correct_shortcut_id, question_genre_id
     # 解答の正誤をまとめた配列を作る
   end
 end
 
-class CreateQuiz < Quiz
+class CreateQuizId < CreateQuiz
   def initialize(level)
-    super(@client,@quiz_id_array)
+    super()
     @level = level
   end
-  def get_db_quiz_id
-    result = @client.query("SELECT quiz_id FROM quiz WHERE question_level=#{@level}") # paramから受け取ったレベルで検索
-    shuffle_array = result.shuffle!
+
+  def array_push_quiz_id
+    limit = 5 # ここで問題数を設定
+    quiz_id_data = @client.query("SELECT quiz_id FROM quiz WHERE question_level = #{@level} ORDER BY RAND() LIMIT #{limit}")
     # 必要な形へ変換
-    quiz_limit = 5 #ここで問題数を設定
-    data = shuffle_array[1..quiz_limit].map do |row|
-      { id: row['quiz_id']}
+    data = quiz_id_data.map do |row|
+      { id: row['quiz_id'] }
     end
-    @quiz_id_array.push(data.to_json)
+    @quiz_id_array.push(data)
+    data.to_json
+  end
+
+  def push_quiz_contents
+
   end
 end
